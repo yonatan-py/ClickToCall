e39857898db6e512c09ade0daa30492e0e520f43
yoni <yonatan.py@gmail.com>
2023-12-17 11:56:18 +0200

    send server notification with notification

--- server/server.go
+++ server/server.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"context"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
 	"fmt"
 	"google.golang.org/api/option"
 	"log"
-
-	firebase "firebase.google.com/go"
-	"firebase.google.com/go/messaging"
+	"time"
 )
+
+func sendMessage(ctx context.Context, client messaging.Client, registrationToken string) {
+	message := &messaging.Message{
+		Data: map[string]string{
+			"score": "foooooo",
+			"time":  "bar",
+		},
+		Token: registrationToken,
+		Notification: &messaging.Notification{
+			Title: "$GOOG up 1.43% on the day",
+			Body:  "$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.",
+		},
+	}
+
+	response, err := client.Send(ctx, message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Successfully sent message:", response)
+}
 
 func main() {
 
@@ -27,18 +48,9 @@ func main() {
 
 	registrationToken := "cbWMXrTYRg-VQQSoPpRq7T:APA91bGYxuLIdfvTTik_0FVhJ3yG5djtRSUZ6sHgXRBrVd0gGbDskydRDwamNTiUztpk9oc25oXwm3-AviioahOlwAJTn6cxRzxdcFPG3O37Rus2p6RiI6nSiYkVqb4kaYY4FC56cdZM"
 
-	message := &messaging.Message{
-		Data: map[string]string{
-			"score": "6dsdsa",
-			"time":  "2:45",
-		},
-		Token: registrationToken,
+	for true {
+		println("sending message")
+		sendMessage(ctx, *client, registrationToken)
+		time.Sleep(5 * time.Second)
 	}
-
-	response, err := client.Send(ctx, message)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Println("Successfully sent message:", response)
 }
