2ed976cb01200047c4830a48a012434c05d269c7
yoni <yonatan.py@gmail.com>
2024-01-03 11:19:53 +0200

    send code and receive push on call

--- AndroidApp/app/src/main/java/com/uruk/clicktocall/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/uruk/clicktocall/MainActivity.kt
@@ -1,45 +1,44 @@
 package com.uruk.clicktocall
 
-import android.os.Bundle
 import android.Manifest
+import android.content.pm.PackageManager
+import android.os.AsyncTask
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.uruk.clicktocall.ui.theme.ClickToCallTheme
-
-import android.util.Log
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import android.content.pm.PackageManager
-import android.os.Build
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
 import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.firestore.FieldValue
-import com.google.firebase.firestore.firestore
-import com.google.firebase.messaging.messaging
-import com.google.firebase.messaging.remoteMessage
+import com.uruk.clicktocall.ui.theme.ClickToCallTheme
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import java.util.concurrent.atomic.AtomicInteger
-
-import com.uruk.clicktocall.R
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 
 class MainActivity : ComponentActivity() {
 
     private val TAG = "MainActivity"
-    val db = Firebase.firestore
+    var code = ""
+    var token = ""
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission(),
@@ -51,6 +50,47 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        code = "123"
+        setContent {
+            ClickToCallTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val coroutineScope = rememberCoroutineScope()
+                    LaunchedEffect(code, token) {
+                        Log.i(TAG, "code: $code")
+                        Log.i(TAG, "token: $token")
+                        coroutineScope.launch {
+                            SendCodeToServer().execute(token, code)
+                        }
+                    }
+                    ShowCode(code)
+                }
+            }
+        }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            token = task.result
+
+
+            // Log and toast
+            val msg = String.format("token: %s", token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+
+        })
+        askNotificationPermission()
+    }
     private fun askNotificationPermission() {
         // This is only necessary for API level >= 33 (TIRAMISU)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -69,50 +109,64 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ClickToCallTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+}
+
+class SendCodeToServer : AsyncTask<String, Void, Response?>() {
+
+    override fun doInBackground(vararg params: String): Response? {
+        try {
+
+            val url = "https://3e50-77-137-20-18.ngrok-free.app/code"
+            val codePayload = JSONObject()
+            val token = params[0]
+            val code = params[1]
+            codePayload.put("code", code)
+            codePayload.put("androidToken", token)
+            val jsonString = codePayload.toString()
+            val client = OkHttpClient()
+
+            val request = Request.Builder()
+                .url(url)
+                .post(jsonString.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull()))
+                .build()
+
+            client.newCall(request).execute().use { response ->
+                Log.d("response", response.toString())
+                if (!response.isSuccessful) {
+                    throw IOException("Unexpected code $response")
                 }
+
+                for ((name, value) in response.headers) {
+                    println("$name: $value")
+                }
+
+                println(response.body!!.string())
             }
+            return null
+        } catch (e: Exception) {
+            Log.e("error", e.toString())
+            return null
         }
+    }
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-                return@OnCompleteListener
-            }
-
-            // Get new FCM registration token
-            val token = task.result
-
-            // Log and toast
-            val msg = String.format("token: %s", token)
-            Log.d(TAG, msg)
-            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-        })
-        askNotificationPermission()
-   }
+    @Deprecated("Deprecated in Java")
+    override fun onPostExecute(result: Response?) {
+        // Update UI or perform any post-execution tasks here
+        // This method runs on the main thread
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ShowCode(code: String) {
+    Column {
+        Text(code)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     ClickToCallTheme {
-        Greeting("Android")
+        ShowCode(code = "123")
     }
 }
